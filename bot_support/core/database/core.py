43b0c89c6cb6073c6a0951c4fe98382e1c2d0153
chebaev <106076557+chebaev@users.noreply.github.com>
2024-07-30 09:05:50 +0500

    Delete bot_support directory

--- bot_support/core/database/core.py
+++
@@ -1,7 +0,0 @@
-# from core.database.utils.CRUD import CRUDInterface
-from core.database.utils.CRUD import CRUDInterface
-
-crud = CRUDInterface()
-
-if __name__ == '__main__':
-    crud()
--- bot_support/core/state/register.py
+++
@@ -1,8 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-
-class RegAnswer(StatesGroup):
-    telegram_id = State()
-    question = State()
-    answer = State()
-    activity = State()
